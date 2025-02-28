ada71cef36338139910a87bb8a8f96289a42ab91
tatianno <tferreiraalves@gmail.com>
2025-02-28 17:57:58 -0300

    Bump version to 0.2.0 in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as arq:
 
 setup(
     name='ami-data-parser',
-    version='0.1.3',
+    version='0.2.0',
     license='MIT License',
     author='Tatianno Alves',
     long_description=readme,
