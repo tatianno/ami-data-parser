209a542e61e5d660b7f855bd273bfe3b1925e1de
tatianno <tferreiraalves@gmail.com>
2025-02-28 18:28:27 -0300

    Update README with installation instructions and example code; add example main.py and requirements.txt

--- example/main.py
+++ example/main.py
@@ -1,48 +1,18 @@
-# AMI Data Parser
-
-This repository aims to provide a tool to interpret and process data received from the Asterisk Manager Interface (AMI). It identifies and exposes status changes in queues and extensions in real-time, enabling more efficient integration with external systems or monitoring dashboards.
-
-## Key Features
-- Parsing data from the AMI.
-- Detecting status changes in queues and extensions.
-- Exposing processed data in an integration-friendly format.
-
-## Repository
-Github: https://github.com/tatianno/ami-data-parser
-
-## Requirements
-- Python 3.10 +
-
-## Installation
-
-Clone the repository and install the dependencies:
-
-```sh
-pip install -r requirements.txt
-```
-
-### Dependencies
-
-The project requires the following packages:
-- `pyst2`
-- `ami-data-parser`
-
-## Example Code
-
-```python
 import sys
 from time import sleep
 from ami_data_parser import Controller
 from asterisk.manager import (
-   Manager,
-   ManagerAuthException,
-   ManagerException,
+   Manager, 
+   ManagerAuthException, 
+   ManagerException, 
    ManagerSocketException
 )
+
 
 AMI_HOST = '192.168.0.103'
 AMI_USERNAME = 'DEV'
 AMI_PASSWORD = 'gnew'
+
 
 if __name__ == '__main__':
 
@@ -84,5 +54,3 @@ if __name__ == '__main__':
 
    finally:
       manager.logoff()
-```
-
