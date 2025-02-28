f7dd681bd93269a0bdae48e040ecc2a8bba8c507
tatianno <tferreiraalves@gmail.com>
2025-02-28 14:57:26 -0300

    Add ChannelParser class with methods to parse channel data

---
+++ ami_data_parser/parser/channel_parser.py
@@ -0,0 +1,66 @@
+class ChannelParser:
+
+    def _get_parser_channel(self, line: str) -> dict:
+
+        if 'Output:' not in line:
+            return None 
+        
+        data = self._get_data_list(line)
+        return {
+            'channel': self._get_channel(data),
+            'context': self._get_context(data),
+            'exten': self._get_exten(data),
+            'priority': self._get_priority(data),
+            'state': self._get_state(data),
+            'application': self._get_application(data),
+            'app_data': self._get_app_data(data),
+            'callerid_num': self._get_callerid_num(data),
+            'uniqueid': self._get_uniqueid(data)
+        }
+    
+    def _get_channel(self, data: list) -> str:
+        return self._get_value_in_list(data, 0)
+    
+    def _get_context(self, data: list) -> str:
+        return self._get_value_in_list(data, 1)
+    
+    def _get_exten(self, data: list) -> str:
+        return self._get_value_in_list(data, 2)
+    
+    def _get_priority(self, data: list) -> str:
+        return self._get_value_in_list(data, 3)
+    
+    def _get_state(self, data: list) -> str:
+        return self._get_value_in_list(data, 4)
+    
+    def _get_application(self, data: list) -> str:
+        return self._get_value_in_list(data, 5)
+    
+    def _get_app_data(self, data: list) -> str:
+        return self._get_value_in_list(data, 6)
+    
+    def _get_callerid_num(self, data: list) -> str:
+        return self._get_value_in_list(data, 7)
+    
+    def _get_uniqueid(self, data: list) -> str:
+        return self._get_value_in_list(data, 13)
+
+    def _get_data_list(self, line: str) -> list:
+        line = self._clean_data(line)
+        return line.split('!')
+    
+    def _clean_data(self, line: str) -> str:
+        remove_itens = ['Output:', '\r', '\n']
+
+        for item in remove_itens:
+            line = line.replace(item, '')
+
+        return line.strip()
+    
+    def _get_value_in_list(self, data: list, index: int) -> str:
+
+        try:
+            return data[index]
+
+        except IndexError:
+            return None
---
+++ ami_data_parser/tests/test_channel_parser.py
@@ -0,0 +1,128 @@
+from unittest import TestCase
+from ami_data_parser.parser.channel_parser import ChannelParser
+
+
+class ChannelParserTestCase(TestCase):
+
+    def test_get_data(self):
+        received_data = ['Response: Success\r\n', 'ActionID: tatianno-ThinkPad-L470-00000001\r\n', 'Message: Command output follows\r\n', 'Output: SIP/IP-7436-QPQGE-00000002!C_1_ENT!997799298!1!Down!AppDial!(Outgoing Line)!997799298!3!3!3!2!!1740760337.2\r\n', 'Output: SIP/IP-101-Q14V2-00000001!macro-externa!s!22!Ring!Dial!SIP/IP-7436-QPQGE/997799298,90,TtM(saida_atendimento)!101!3!!3!2!!1740760337.1\r\n']
+    
+
+    def test_get_parser_channel(self):
+        received_data = 'Output: SIP/IP-7436-QPQGE-00000002!C_1_ENT!997799298!1!Down!AppDial!(Outgoing Line)!997799298!3!3!3!2!!1740760337.2\r\n'
+        expected_data = {
+            'channel': 'SIP/IP-7436-QPQGE-00000002',
+            'context': 'C_1_ENT',
+            'exten': '997799298',
+            'priority': '1',
+            'state': 'Down',
+            'application': 'AppDial',
+            'app_data': '(Outgoing Line)',
+            'callerid_num': '997799298',
+            'uniqueid': '1740760337.2'
+        }
+        parser = ChannelParser()
+        data = parser._get_parser_channel(received_data)
+        self.assertEqual(data, expected_data)
+
+    def test_get_parse_channel_invalid_data(self):
+        received_data = 'Response: Success\r\n'
+        expected_data = None
+        parser = ChannelParser()
+        data = parser._get_parser_channel(received_data)
+        self.assertEqual(data, expected_data)
+
+    def test_get_channel(self):
+        received_data = ['SIP/IP-7436-QPQGE-00000002','C_1_ENT','997799298','1','Down','AppDial','(Outgoing Line)','997799298','3','3','3','2','','1740760337.2']
+        expected_data = 'SIP/IP-7436-QPQGE-00000002'
+        parser = ChannelParser()
+        channel = parser._get_channel(received_data)
+        self.assertEqual(channel, expected_data)
+    
+    def test_get_context(self):
+        received_data = ['SIP/IP-7436-QPQGE-00000002','C_1_ENT','997799298','1','Down','AppDial','(Outgoing Line)','997799298','3','3','3','2','','1740760337.2']
+        expected_data = 'C_1_ENT'
+        parser = ChannelParser()
+        context = parser._get_context(received_data)
+        self.assertEqual(context, expected_data)
+    
+    def test_get_exten(self):
+        received_data = ['SIP/IP-7436-QPQGE-00000002','C_1_ENT','997799298','1','Down','AppDial','(Outgoing Line)','997799298','3','3','3','2','','1740760337.2']
+        expected_data = '997799298'
+        parser = ChannelParser()
+        exten = parser._get_exten(received_data)
+        self.assertEqual(exten, expected_data)
+    
+    def test_get_priority(self):
+        received_data = ['SIP/IP-7436-QPQGE-00000002','C_1_ENT','997799298','1','Down','AppDial','(Outgoing Line)','997799298','3','3','3','2','','1740760337.2']
+        expected_data = '1'
+        parser = ChannelParser()
+        priority = parser._get_priority(received_data)
+        self.assertEqual(priority, expected_data)
+    
+    def test_get_state(self):
+        received_data = ['SIP/IP-7436-QPQGE-00000002','C_1_ENT','997799298','1','Down','AppDial','(Outgoing Line)','997799298','3','3','3','2','','1740760337.2']
+        expected_data = 'Down'
+        parser = ChannelParser()
+        state = parser._get_state(received_data)
+        self.assertEqual(state, expected_data)
+    
+    def test_get_application(self):
+        received_data = ['SIP/IP-7436-QPQGE-00000002','C_1_ENT','997799298','1','Down','AppDial','(Outgoing Line)','997799298','3','3','3','2','','1740760337.2']
+        expected_data = 'AppDial'
+        parser = ChannelParser()
+        application = parser._get_application(received_data)
+        self.assertEqual(application, expected_data)
+    
+    def test_get_app_data(self):
+        received_data = ['SIP/IP-7436-QPQGE-00000002','C_1_ENT','997799298','1','Down','AppDial','(Outgoing Line)','997799298','3','3','3','2','','1740760337.2']
+        expected_data = '(Outgoing Line)'
+        parser = ChannelParser()
+        app_data = parser._get_app_data(received_data)
+        self.assertEqual(app_data, expected_data)
+    
+    def test_get_callerid_num(self):
+        received_data = ['SIP/IP-7436-QPQGE-00000002','C_1_ENT','997799298','1','Down','AppDial','(Outgoing Line)','997799298','3','3','3','2','','1740760337.2']
+        expected_data = '997799298'
+        parser = ChannelParser()
+        callerid_num = parser._get_callerid_num(received_data)
+        self.assertEqual(callerid_num, expected_data)
+    
+    def test_get_uniqueid(self):
+        received_data = ['SIP/IP-7436-QPQGE-00000002','C_1_ENT','997799298','1','Down','AppDial','(Outgoing Line)','997799298','3','3','3','2','','1740760337.2']
+        expected_data = '1740760337.2'
+        parser = ChannelParser()
+        callerid_num = parser._get_uniqueid(received_data)
+        self.assertEqual(callerid_num, expected_data)
+    
+    def test_get_uniqueid_raise_except_index_error(self):
+        received_data = ['SIP/IP-7436-QPQGE-00000002','C_1_ENT','997799298']
+        expected_data = None
+        parser = ChannelParser()
+        callerid_num = parser._get_uniqueid(received_data)
+        self.assertEqual(callerid_num, expected_data)
+    
+    def test_get_data_list(self):
+        received_data = 'Output: SIP/IP-7436-QPQGE-00000002!C_1_ENT!997799298!1!Down!AppDial!(Outgoing Line)!997799298!3!3!3!2!!1740760337.2\r\n'
+        expected_data = ['SIP/IP-7436-QPQGE-00000002','C_1_ENT','997799298','1','Down','AppDial','(Outgoing Line)','997799298','3','3','3','2','','1740760337.2']
+        parser = ChannelParser()
+        data_list = parser._get_data_list(received_data)
+        self.assertListEqual(data_list, expected_data)
+    
+    def test_clean_data(self):
+        received_data = 'Output: SIP/IP-7436-QPQGE-00000002!C_1_ENT!997799298!1!Down!AppDial!(Outgoing Line)!997799298!3!3!3!2!!1740760337.2\r\n'
+        expected_data = 'SIP/IP-7436-QPQGE-00000002!C_1_ENT!997799298!1!Down!AppDial!(Outgoing Line)!997799298!3!3!3!2!!1740760337.2'
+        parser = ChannelParser()
+        data = parser._clean_data(received_data)
+        self.assertEqual(data, expected_data)
+    
+    def test_get_value_in_list(self):
+        received_data = ['SIP/IP-7436-QPQGE-00000002','C_1_ENT','997799298','1','Down','AppDial','(Outgoing Line)','997799298','3','3','3','2','','1740760337.2']
+        expected_data = 'Down'
+        parser = ChannelParser()
+        data = parser._get_value_in_list(received_data, 4)
+        self.assertEqual(data, expected_data)
+        expected_data = None
+        data = parser._get_value_in_list(received_data, 15)
+        self.assertEqual(data, expected_data)
+        
